ce4f7ca698fb65be2654c15543220824bc676f4b
aviboddu <aviboddu@gmail.com>
2023-05-23 13:42:27 -0700

    Added simple objective transition - fade in and fade out

--- Assets/Scripts/ObjectiveText.cs
+++ Assets/Scripts/ObjectiveText.cs
@@ -1,5 +1,6 @@
 using TMPro;
 using UnityEngine;
+using System.Collections;
 
 public class ObjectiveText : MonoBehaviour
 {
@@ -14,6 +15,24 @@ public class ObjectiveText : MonoBehaviour
     // Called whenever the objective is updated.
     private void UpdateText(int currentObjectiveNumber)
     {
+        StartCoroutine(transitionObjectiveName());
+    }
+
+    private IEnumerator transitionObjectiveName(float t1 = 0.5f, float t2 = 0.5f)
+    {
+        StartCoroutine(fade(_objectiveText.alpha, 0, t1));
+        yield return new WaitForSeconds(t1);
         _objectiveText.text = ObjectiveManager.Manager.GetCurrentObjectiveText();
+        StartCoroutine(fade(_objectiveText.alpha, 1, t2));
+    }
+
+    private IEnumerator fade(float initial, float final, float t)
+    {
+        for (float time = 0; time < t; time += Time.deltaTime)
+        {
+            _objectiveText.alpha = initial + (final - initial) * (time / t);
+            yield return null;
+        }
+        _objectiveText.alpha = final;
     }
 }
